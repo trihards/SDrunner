6ad75734b890d2a133e64a576d7445ef5e79cb9a
trihards <trihards@users.noreply.github.com>
2016-10-04 07:26:47 -0700

    SD runner
    via Lynda.com

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ObjectPool : MonoBehaviour {
+
+	public RecycleGameObject prefab;
+
+	private List<RecycleGameObject> poolInstance = new List<RecycleGameObject>();
+
+	private RecycleGameObject CreateInstace(Vector3 pos) {
+	
+		var clone = GameObject.Instantiate (prefab);
+		clone.transform.position = pos;
+		clone.transform.parent = transform;
+
+		poolInstance.Add (clone);
+
+		return clone;
+
+			
+	}
+
+	public RecycleGameObject NextObject(Vector3 pos){
+		RecycleGameObject instance = null;
+
+		foreach (var go in poolInstance) {
+			if (go.gameObject.activeSelf != true) {
+				instance = go;
+				instance.transform.position = pos;
+
+			}
+		}
+
+		if(instance == null)
+			instance = CreateInstace (pos);
+
+		instance.Restart ();
+
+		return instance;
+	}
+
+}
